969e3e920167f1a3c68d3bc8caca6752f5260e09
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-02-08 11:50:04 +0200

    add index, js files

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let a = prompt('First number?/Перше число?', 2)
+let b = prompt('Second number?/Друге число?', 2)
+
+alert(`Addition/Сума: ${Number(a) + Number(b)}`)
+alert(`Subtraction/Різниця: ${Number(a) - (b)}`)
+alert(`Multiplication/Добуток: ${Number(a) * (b)}`)
+alert(`Division/Частка: ${Number(a) / (b)}`)
