7b702d2a20ee46f4f456290b60a96b996ff2856f
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-02-20 13:46:35 +0200

    add js file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+    constructor(options) {
+        this.name = options.name;
+        this.age = options.age;
     }
     get personInfo() {
         console.log(`Person: ${this.name}, ${this.age}`)
@@ -10,11 +10,11 @@ class Person {
 }
 
 class Car {
-    constructor(brand, model, year, number) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.number = number;
+    constructor(options) {
+        this.brand = options.brand;
+        this.model = options.model;
+        this.year = options.year;
+        this.number = options.number;
     }
     owner(owner) {
         if (owner.age < 18) {
@@ -29,19 +29,19 @@ class Car {
     }
 }
 
-const molly = new Person('Molly', 25);
-const robert = new Person('Robert', 17);
-const bettie = new Person('Bettie', 45);
-const paul = new Person('Paul', 45);
+const molly = new Person({ name: 'Molly', age: 25 });
+const robert = new Person({ name: 'Robert', age: 17 });
+const bettie = new Person({ name: 'Bettie', age: 45 });
+const paul = new Person({ name: 'Paul', age: 45 });
 console.log(molly);
 console.log(robert);
 console.log(bettie);
 console.log(paul);
 
-const tesla = new Car('Tesla', 'Model S', 2023, 3147);
-const audi = new Car('Audi', 'Q5', 2022, 5874);
-const toyota = new Car('Toyota', 'Avensis', 2020, 2587);
-const scoda = new Car('Scoda', 'Fabia', 2008, 1593);
+const tesla = new Car({ brand: 'Tesla', model: 'Model S', year: 2023, number: 3147 });
+const audi = new Car({ brand: 'Audi', model: 'Q5', year: 2022, number: 5874 });
+const toyota = new Car({ brand: 'Toyota', model: 'Avensis', year: 2020, number: 2587 });
+const scoda = new Car({ brand: 'Scoda', model: 'Fabia', year: 2008, number: 1593 });
 console.log(tesla)
 console.log(audi);
 console.log(toyota);
