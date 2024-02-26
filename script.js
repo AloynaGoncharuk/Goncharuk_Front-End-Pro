41695e64c9ec0c7497eb509ca3555ae7047a3d62
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-02-26 17:27:23 +0200

    add index, js files

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Version 1
+const numbers = [24, 122, 55, 13, 3, 88]
+numbers.sort(function (a, b) {
+    return a - b;
+}
+);
+console.log(numbers)
 
+numbers.splice(2, 3);
+console.log(numbers)
+
+// Version 2
+const cities = ['London', 'Kyiv', 'Paris', 'Berlin', 'Warsaw', 'Bratislava']
+cities.sort();
+console.log(cities)
+
+cities.splice(2, 3);
+console.log(cities)
