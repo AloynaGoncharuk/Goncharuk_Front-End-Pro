06a2038ce1d760accd266b5588efa1b1ca2e783f
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-02-26 22:10:17 +0200

    add js file

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// Version 1
-const numbers = [24, 122, 55, 13, 3, 88]
+
+const numbers = [];
+for (i = 0; i < 10; i++) {
+    numbers[i] = prompt('Введіть елемент масиву');
+
+    if (numbers[i] === null) {
+        console.log('Помилка');
+    }
+    if (numbers[i] === '') {
+        console.log('Ви не вказали елемент');
+    }
+}
+console.log(numbers);
+
 numbers.sort(function (a, b) {
     return a - b;
 }
 );
-console.log(numbers)
+console.log(numbers);
 
 numbers.splice(2, 3);
 console.log(numbers)
 
-// Version 2
-const cities = ['London', 'Kyiv', 'Paris', 'Berlin', 'Warsaw', 'Bratislava']
-cities.sort();
-console.log(cities)
-
-cities.splice(2, 3);
-console.log(cities)
