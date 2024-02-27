302beb75ef760642a54e2a8528d08026499bd2d2
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-02-27 17:34:18 +0200

    add js file

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
-const numbers = [];
-for (i = 0; i < 10; i++) {
+const arr = [];
+const c = prompt('Введіть кількість елементів масиву');
+console.log(c);
+
+const numbers = new Array(6);
+for (i = 0; i < 6; i++) {
     numbers[i] = prompt('Введіть елемент масиву');
 
     if (numbers[i] === null) {
@@ -18,6 +22,5 @@ numbers.sort(function (a, b) {
 );
 console.log(numbers);
 
-numbers.splice(2, 3);
+numbers.splice(1, 3);
 console.log(numbers)
-
