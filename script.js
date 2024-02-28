c8371456f473d32538901d6f89a42d5a6a91e600
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-02-28 22:19:29 +0200

    add js file

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 class Student {
-    constructor(firstName, lastName, birthDay, marks) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthDay = birthDay;
+    constructor(name, surname, yearBirth, marks) {
+        this.name = name;
+        this.surname = surname;
+        this.yearBirth = yearBirth;
         this.marks = marks;
         this.absence = new Array(25);
         this.absenceIndex = 0;
-        this.age = this.getAge(birthDay);
+        this.age = this.getAge(yearBirth);
         this.averageMark = this.marks.reduce((r, m) => r + m) / this.marks.length;
-        this.presenceFactor = 0.9;
+        this.presenceIndex = 0.9;
         this.goodMarksMin = 90;
         this.results = {
             BAD: "Редиска!",
@@ -18,54 +18,48 @@ class Student {
         };
     }
 
-    absent() {
-        if (this.absence.length > this.absenceIndex) {
-            this.absence[this.absenceIndex] = false;
-            this.absenceIndex++;
-        }
-    };
-
     present() {
         if (this.absence.length > this.absenceIndex) {
-            this.absence[this.absenceIndex] = true;
-            this.absenceIndex++;
+            this.absence = true;
         }
     };
 
-    get avaragePresence() {
-        var precenceCount = this.absence.slice(0, this.absenceIndex).filter(x => x).length;
-        return precenceCount / this.absenceIndex;
-    }
+    absent() {
+        if (this.absence.length > this.absenceIndex) {
+            this.absence = false;
+        }
+    };
 
     getAge(dateString) {
-        var today = new Date();
-        var birthDate = new Date(dateString);
-        var age = today.getFullYear() - birthDate.getFullYear();
-        var m = today.getMonth() - birthDate.getMonth();
+        const today = new Date();
+        const birthDate = new Date(dateString);
+        const age = today.getFullYear() - birthDate.getFullYear();
+        const m = today.getMonth() - birthDate.getMonth();
         if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
+            age;
         }
         return age;
     }
 
     summary() {
-        if (this.averageMark < this.goodMarksMin && this.avaragePresence < this.presenceFactor) {
+        if (this.averageMark < this.goodMarksMin && this.avaragePresence < this.presenceIndex) {
             console.log(this.results.BAD);
-        } else if (this.averageMark < this.goodMarksMin || this.avaragePresence < this.presenceFactor)
+        } else if (this.averageMark < this.goodMarksMin || this.avaragePresence < this.presenceIndex)
             console.log(this.results.NORMAL);
         else
             console.log(this.results.GOOD);
     }
 }
-const molly = new Student('Molly', 'Robertson', '01/20/1993', [90, 60, 90]);
-const nikolas = new Student('Nikolas', 'Caidge', '03/28/1986', [90, 80, 90]);
-console.log(molly);
-console.log(nikolas);
 
+const molly = new Student('Molly', 'Robertson', '01/20/1993', [96, 92, 93]);
+console.log(molly);
 molly.present();
 molly.absent();
 molly.summary();
 
+const nikolas = new Student('Nikolas', 'Caidge', '03/28/1986', [85, 80, 86]);
+console.log(nikolas);
 nikolas.present();
 nikolas.absent();
 nikolas.summary();
+
