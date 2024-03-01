d4cbc29bc611dc0c2d4972a99f860bd9cea32f72
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-03-01 15:16:08 +0200

    add index, js files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+function generateKey(length) {
+    let characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let charLength = characters.length;
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charLength));
+    }
+    return result;
+}
+console.log(generateKey(16));
+
