024a5bcbca95579d8233341a6cf151e09a61c490
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-03-17 11:58:17 +0200

    add index, js, css files

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let name = document.querySelector('#name');
+let surname = document.querySelector('#surname');
+let birth = document.querySelector('#birth');
+let submit = document.querySelector('#dsubmit');
