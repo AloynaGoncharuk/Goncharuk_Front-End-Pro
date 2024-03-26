c28ddfae228e5ca2487661369af7e3501b544a60
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-03-26 13:57:35 +0200

    add index, js, css files

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 
-let name = document.querySelector('#name');
-let surname = document.querySelector('#surname');
-let birth = document.querySelector('#birth');
-let submit = document.querySelector('#dsubmit');
+const table = document.getElementById("employeeList");
+let row = table.insertRow(-1);
+
+document.getElementById('submit').addEventListener('click', () => {
+    document.getElementById("employeeList").style.display = "block";
+    let name = row.insertCell(0);
+    name.innerHTML = document.getElementById("name").value;
+});
+
+document.getElementById('submit').addEventListener('click', () => {
+    document.getElementById("employeeList").style.display = "block";
+    let surname = row.insertCell(1);
+    surname.innerHTML = document.getElementById("surname").value;
+});
+
+document.getElementById('submit').addEventListener('click', () => {
+    document.getElementById("employeeList").style.display = "block";
+    let birth = row.insertCell(2);
+    birth.innerHTML = document.getElementById("birth").value;
+});
+
