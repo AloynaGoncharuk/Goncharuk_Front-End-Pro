a115a17695ed3bcb65b153d35aa5c35a52fcd22f
AloynaGoncharuk <ang.aloyna@gmail.com>
2024-03-27 23:31:46 +0200

    add index, js, css files

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 
-const table = document.getElementById("employeeList");
-let row = table.insertRow(-1);
-
 document.getElementById('submit').addEventListener('click', () => {
-    document.getElementById("employeeList").style.display = "block";
-    let name = row.insertCell(0);
-    name.innerHTML = document.getElementById("name").value;
+    document.querySelector(".list").style.display = "block";
+    document.getElementById("form").style.display = "none";
+    let name = document.getElementById('name').value;
+    let surname = document.getElementById('surname').value;
+    let birth = document.getElementById('birth').value;
+    let selectElement = document.getElementById('city').value;
+    let textarea = document.getElementById('adress').value;
+
+    let gender = document.getElementsByName('gender');
+    for (let i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            document.getElementById('showGender').innerHTML = gender[i].value;
+        }
+    }
+
+    let checkboxes = document.getElementsByName('lang');
+    let result = "";
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            result += checkboxes[i].value + ' ,' + ' ';
+            document.getElementById('showLanguage').innerHTML = result;
+        }
+    }
+    document.getElementById('showName').innerHTML = name;
+    document.getElementById('showSurname').innerHTML = surname;
+    document.getElementById('showBirth').innerHTML = birth;
+    document.getElementById('showCity').innerHTML = selectElement;
+    document.getElementById('showAdress').innerHTML = textarea;
+
 });
 
-document.getElementById('submit').addEventListener('click', () => {
-    document.getElementById("employeeList").style.display = "block";
-    let surname = row.insertCell(1);
-    surname.innerHTML = document.getElementById("surname").value;
-});
 
-document.getElementById('submit').addEventListener('click', () => {
-    document.getElementById("employeeList").style.display = "block";
-    let birth = row.insertCell(2);
-    birth.innerHTML = document.getElementById("birth").value;
-});
 
